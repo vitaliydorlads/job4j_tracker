0b5f771789670463956545de91df7fdf590a7d1a
виталийдорладс <vitaliydorlads@gmail.com>
2024-02-13 13:16:41 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Каруфин Виталий Александрович");
+        student.setGroup("ЗПЭ - 462");
+        student.setDateOfReceipt("13.02.2024");
+        System.out.println(student.getFullName() + "\nГруппа: " + student.getGroup() + "\nДата поступления: " + student.getDateOfReceipt());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Student {
+    private String fullName;
+    private String group;
+    private String dateOfReceipt;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getDateOfReceipt() {
+        return dateOfReceipt;
+    }
+
+    public void setDateOfReceipt(String dateOfReceipt) {
+        this.dateOfReceipt = dateOfReceipt;
+    }
+}
