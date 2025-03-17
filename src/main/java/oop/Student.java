946b43a093cb40a9965fe54724787b18896f4fc6
Vitaliy <vitaliydorlads@gmail.com>
2025-03-17 21:44:38 +0300

    1.2. Вызов метода объекта [#166085 #465708]

---
+++ src/main/java/oop/Student.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class Student {
+    public void sing() {
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student pety = new Student();
+        pety.sing();
+        pety.sing();
+        pety.sing();
+    }
+}
