f22ff418357f438279bcb3ca1248c68714367809
Vitaliy <vitaliydorlads@gmail.com>
2025-03-17 19:26:46 +0300

    6. Правила Checkstyle [#504981 #464620]

---
+++ src/main/java/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    private String surname;
+    private String name;
+    private static final String NEW_VALUE = "";
+
+    public Broken() {
+
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+}
+
