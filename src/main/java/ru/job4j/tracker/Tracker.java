0360b0a4dab833877d93ff8b73cf9c8c1f1e61cc
Vitaliy <vitaliydorlads@gmail.com>
2025-03-20 07:09:33 +0300

    Перегрузка конструктора [#173890 #467285]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -4,19 +4,6 @@ public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
-    private String name;
-
-    public Tracker() {
-    }
-
-    public Tracker(String name) {
-        this.name = name;
-    }
-
-    public Tracker(int ids, String name) {
-        this.ids = ids;
-        this.name = name;
-    }
 
     public Item add(Item item) {
         item.setId(ids++);
