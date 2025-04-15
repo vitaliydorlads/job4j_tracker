66a057496c957d499a97d4982024029265153fdb
Vitaliy <vitaliydorlads@gmail.com>
2025-04-15 11:29:34 +0300

    12.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -15,4 +15,4 @@ public class ConsoleInput implements Input {
     public int askInt(String question) {
         return Integer.parseInt(askStr(question));
     }
-}
+}.
--- src/main/java/ru/job4j/tracker/StubOutput.java
+++ src/main/java/ru/job4j/tracker/StubOutput.java
@@ -17,4 +17,4 @@ public class StubOutput implements Output {
     public String toString() {
         return buffer.toString();
     }
-}юь
+}.
