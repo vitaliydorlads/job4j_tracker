b4b844dcc53c9a03ad006984d7b1c9d50a0190b2
Vitaliy <vitaliydorlads@gmail.com>
2025-04-22 19:47:37 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,6 +6,7 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 result = i;
+                break;
             }
         }
         if (result == -1) {
