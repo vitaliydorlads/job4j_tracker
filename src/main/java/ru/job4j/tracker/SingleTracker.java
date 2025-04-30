2b8ff1f339ade9cdbef28399c0310d15106e8807
Vitaliy <vitaliydorlads@gmail.com>
2025-05-01 00:02:01 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+    private static SingleTracker instance = null;
+    private final Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    private int indexOf(int id) {
+        return tracker.indexOf(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+}
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -33,7 +33,7 @@ public class Tracker {
         return index != -1 ? items[index] : null;
     }
 
-    private int indexOf(int id) {
+    public int indexOf(int id) {
         int result = -1;
         for (int index = 0; index < size; index++) {
             if (items[index].getId() == id) {
