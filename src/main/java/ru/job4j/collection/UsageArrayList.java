e44c29044ee337555cc187ace055fd901f10c7b9
Vitaliy <vitaliydorlads@gmail.com>
2025-05-09 16:51:51 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<>();
+        name.add("Petr");
+        name.add("Ivan");
+        name.add("Stepan");
+        for (Object index : name) {
+            System.out.println(index);
+        }
+    }
+}
