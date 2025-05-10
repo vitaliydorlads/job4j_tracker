71aee2bcb341a63afd562d4ce7741f9dbb1ceab3
Vitaliy <vitaliydorlads@gmail.com>
2025-05-11 00:14:51 +0300

    4. Телефонный справочник на базе ArrayList [#41598]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -9,12 +9,22 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class PhoneDictionaryTest {
 
     @Test
-    void find() {
+    void whenFindByName() {
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    void whenFindByPhoneThenNotObj() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("977889");
+        assertThat(persons.isEmpty()).isTrue();
     }
 }
