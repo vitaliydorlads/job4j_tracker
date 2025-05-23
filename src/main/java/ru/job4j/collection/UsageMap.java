29772c8fd3460885ad551815d980af6aa7d1270a
Vitaliy <vitaliydorlads@gmail.com>
2025-05-23 14:20:37 +0300

    0. Map, HashMap [#212652 #544282]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("mail@mail.ru", "Денисов Иван Иванович");
+        map.put("mail@mail.ru", "Denisov Ivan");
+        map.put("qaaaqqq@bk.ru", "Kashin Dmitriy");
+        map.put("yandex@mail.ru", "Kabanob Ilia");
+        map.put("poshta@bk.ru", "Ivanteev Vasia");
+        map.put("poshta@bk.ru", "Prokofieva Svetlana");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " " + value);
