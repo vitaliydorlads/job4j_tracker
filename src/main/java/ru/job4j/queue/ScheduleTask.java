d45df2ef8b88b317233e4a39d4ed76c9d55d19b7
Vitaliy <vitaliydorlads@gmail.com>
2025-06-05 21:23:28 +0300

    5. PriorityQueue [#504944]

---
+++ src/main/java/ru/job4j/queue/ScheduleTask.java
@@ -0,0 +1,28 @@
+package ru.job4j.queue;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class ScheduleTask {
+    private PriorityQueue<Task> queue;
+
+    public ScheduleTask(Comparator<Task> comparator) {
+        this.queue = new PriorityQueue<>(comparator);
+    }
+
+    public void addTask(Task task) {
+        if (task == null) {
+            System.out.println("Невозможно добавить пустую задачу");
+            return;
+        }
+        queue.offer(task);
+    }
+
+    public Task readTask() {
+        return queue.peek();
+    }
+
+    public Task getTask() {
+        return queue.poll();
+    }
+}
