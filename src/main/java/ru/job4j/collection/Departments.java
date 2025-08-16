4b85c42276003793b895866139b54d63f4cc0f3f
Vitaliy <vitaliydorlads@gmail.com>
2025-08-16 23:05:02 +0300

    3. Отсортировать департаменты [#34352]3880 #546254]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> departments) {
+        Set<String> temp = new LinkedHashSet<>();
+        for (String value : departments) {
+            String start = "";
+            for (String element : value.split("/")) {
+                if (start.isEmpty()) {
+                    start = element;
+                } else {
+                    start = start + "/" + element;
+                }
+                temp.add(start);
+            }
+        }
+        return new ArrayList<>(temp);
+    }
+
+    public static void sortAsc(List<String> departments) {
+        departments.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> departments) {
+     departments.sort(new DepartmentsDescComparator());
+    }
+}
